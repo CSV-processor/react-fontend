64c779eda5530f9cbb9bd0771eaad40af903ea96
LEv145 <you@example.com>
2023-02-24 02:48:29 +0200

    v 0.2.5 Add module: `BemClassname` Update module: `ClassNameComponent`

--- src/components/ToolsMenu/component.jsx
+++ src/components/ToolsMenu/component.jsx
@@ -8,15 +8,9 @@ import "./component.sass"
 export default class ToolsMenu extends ClassNameComponent {
     render = () => (
         <div className={this.className}>
-<<<<<<< HEAD
-            <Button variant="contained" className="tools-menu__button">Import</Button>
-            <Button variant="contained" className="tools-menu__button">Save</Button>
-            <Button variant="contained" className="tools-menu__button">Export</Button>
-=======
             <Button variant="contained" className={this.cn("button")()}>Import</Button>
             <Button variant="contained" className={this.cn("button")()}>Save</Button>
             <Button variant="contained" className={this.cn("button")()}>Export</Button>
->>>>>>> fa2923b (v 0.2.5)
         </div>
     )
 }
--- src/components/UserMenu/component.jsx
+++ src/components/UserMenu/component.jsx
@@ -6,11 +6,7 @@ import "./component.sass"
 export default class UserMenu extends ClassNameComponent {
     render = () => (
         <div className={this.className}>
-<<<<<<< HEAD
-            <div className="user-menu__title">Login</div>
-=======
             <div className={this.cn("title")()}>Login</div>
->>>>>>> fa2923b (v 0.2.5)
         </div>
     )
 }
